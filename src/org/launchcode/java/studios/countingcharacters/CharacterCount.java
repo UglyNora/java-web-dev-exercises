4c545cc4ae280c924bf68b88ef72fdb5a47b524e
uglynora <mangos8@yahoo.com>
2020-06-22 20:01:39 -0400

    Modified CharacterCount.

--- src/org/launchcode/java/studios/countingcharacters/CharacterCount.java
+++ src/org/launchcode/java/studios/countingcharacters/CharacterCount.java
@@ -1,9 +1,9 @@
 package org.launchcode.java.studios.countingcharacters;
-import java.util.*;
+//import java.util.*;
 public class CharacterCount {
 
     public static void main (String[] args) {
-     HashMap<Character, Integer> movieQuote = new HashMap<>();
+
      String myString = "If the product of two terms is zero" +
              " then common sense says at least one of the two terms" +
              " has to be zero to start with. " +
@@ -11,22 +11,36 @@ public class CharacterCount {
              "you can put the quadratics into a form that can be factored" +
              " allowing that side of the equation to equal zero. " +
              "Once you’ve done that, it’s pretty straightforward from there.";
-     int count = 0;
-     char[] charactersInString = myString.toCharArray();
+     int matches = 0;
+     int length = myString.length();
 
-     for(char ch: charactersInString) {
-        char nextChar = ch;
-        count ++;
-         if (ch == nextChar) {
-
-             movieQuote.put(ch, count);
-
-         }
+     //Convert given string to an array of characters.
+     int[] numChars = new int[300];
 
 
-     }
+     for(int i = 0; i < length; i++)
+         numChars[myString.charAt(i)] ++;
+
+
+         //Initialize another array
+      char [] charArray  = myString.toCharArray();
+      for ( int i = 0; i < length; i++) {
+          charArray[i] = myString.charAt(i);
+          for (int j =0; j <=i; j++) {
+              if (myString.charAt(i) == charArray[j]) {
+                  matches ++;
+
+              }
+              if (matches == 1)
+                  System.out.println(myString.charAt(i) + ":" + numChars[myString.charAt(i)]);
+
+          }
+      }
 
 
 
-    }
+
+
+
+}
 }
